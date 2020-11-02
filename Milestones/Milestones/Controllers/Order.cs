ddae924673c2c412dc57b0826e43b150d143ce98
Nurzhan Nauryzbayev <60682212+iamnurzh@users.noreply.github.com>
2020-11-02 22:03:48 +0600

    Add files via upload

---
+++ Milestones/Milestones/Controllers/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Milestones.Controllers
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string User { get; set; }
+        public string Address { get; set; }
+        public string ContactPhone { get; set; }
+    }
+}
