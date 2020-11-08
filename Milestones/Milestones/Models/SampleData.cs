360582e09c1c53e45160d05d89de03661e606a4d
Nurzhan Nauryzbayev <60682212+iamnurzh@users.noreply.github.com>
2020-11-08 14:44:43 +0600

    Add files via upload

---
+++ Milestones/Milestones/Models/SampleData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Milestones.Models
+{
+    public class SampleData
+    {
+        public static void Initialize(Milestones milestones)
+        {
+            if (!milestones.Jewelry.Any())
+            {
+                milestones.Jewelry.AddRange(
+                    new Jewelry
+                    {
+                        Name = "Huggie",
+                        Price = 600,
+                    },
+
+                    new Jewelry
+                    {
+                        Name = "Chain",
+                        Price = 550,
+                    },
+                    new Jewelry
+                    {
+                        Name = "Charm",
+                        Price = 500
+                    }
+                );
+                milestones.SaveChanges();
+            }
+        }
+    }
+}
